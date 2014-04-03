3b3043e2a6e12456221b635972548ff9df12598e
harimohanraj89 <hari.mohanraj89@gmail.com>
2014-04-03 15:08:08 -0400

    final tweaks before branching to refactor

--- app/controllers/rt_api_controller.rb
+++ app/controllers/rt_api_controller.rb
@@ -6,15 +6,15 @@ class RtApiController < ApplicationController
 	end
 
 	def search
-		query = params[:q].downcase.split.join('+')
-		raw_response = HTTParty.get("http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=#{URI.escape(query)}&page_limit=10&apikey=#{ENV["ROTTEN_TOMATOES_API_KEY"]}")
+		@query = params[:q].downcase.split.join('+')
+		@raw_response = HTTParty.get("http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=#{URI.escape(query)}&page_limit=10&apikey=#{ENV["ROTTEN_TOMATOES_API_KEY"]}")
     response = JSON.parse(raw_response)
     @results = response['movies']
 	end
 
   def movies
-    movie_id = params[:id]
-    raw_response = HTTParty.get("http://api.rottentomatoes.com/api/public/v1.0/movies/#{movie_id}.json?apikey=#{ENV["ROTTEN_TOMATOES_API_KEY"]}")
+    @movie_id = params[:id]
+    @raw_response = HTTParty.get("http://api.rottentomatoes.com/api/public/v1.0/movies/#{movie_id}.json?apikey=#{ENV["ROTTEN_TOMATOES_API_KEY"]}")
     @movie = JSON.parse(raw_response)
   end
 
